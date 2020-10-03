8528464c0b3183541707bcde2b69e9a63c4a3b1c
Nelson5501 <nelsoncaterino@gmail.com>
2020-10-03 20:59:14 +0200

    premier jeux en c#

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace jeux_de_des
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Voici le Tirage de dés");
+
+            bool continuer = true;
+            Random rand = new Random();
+            while (continuer)
+            {
+                int aleatoire = rand.Next(1, 7);
+                Console.WriteLine("J'ai fait un " + aleatoire);
+
+
+                Console.WriteLine("Voulez-vous continuer ? (oui/non)");
+                string reponce = Console.ReadLine();
+                if (reponce.Equals("non"))
+                {
+                    continuer = false;
+                }
+            }
+        }
+    }
+}
